85333967d37058697c0d57791512ebff7670bf7b
Steven.Dellamore <Steven.Dellamore@partner.bmw.de>
2019-08-12 08:16:12 -0500

    Atom commit test

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,10 +1,8 @@
 package twofer
 
-// ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-    // name = "One for " + name + ", one for me."
-    if name == "" {
-        name = "you"
-    }
+	if name == "" {
+		name = "you"
+	}
 	return "One for " + name + ", one for me."
 }
